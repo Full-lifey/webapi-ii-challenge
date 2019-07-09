bb3076a99a3d440ffdbf55de0480e5427cefe4c2
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-07-09 14:30:13 -0600

    GET posts by ID endpoint finished

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -11,7 +11,30 @@ router.get('/', (req, res) => {
     .then(posts => {
       res.status(200).json(posts);
     })
-    .catch(err => res.status(500).json(err));
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' })
+    );
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+
+    if (post.length) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: 'The post information could not be retrieved.' });
+  }
 });
 
 module.exports = router; // Exports router to add to index.js
