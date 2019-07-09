d2494aaa3047d6359d0ad97213b2c85e2502b30d
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-07-09 16:32:32 -0600

    MVP: PUT /api/posts/:id endpoint completed

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -107,6 +107,28 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  try {
+    if (!isValidPost(req.body)) {
+      res.status(400).json({
+        errorMessage: 'Please provide title and contents for the post.'
+      });
+    } else {
+      const post = await Posts.update(req.params.id, req.body);
+      if (post) {
+        res.status(200).json(await Posts.findById(req.params.id));
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'The post information could not be modified.' });
+  }
+});
 function isValidPost(post) {
   const { title, contents } = post;
   return title && contents;
